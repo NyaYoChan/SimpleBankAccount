5d99ff3b480bbeb4e41838f66e2e095cf6fdd3c5
NyaYo <76841616+NyaYoChan@users.noreply.github.com>
2022-12-19 20:17:19 +0800

    Add files via upload
    2022.12.19 set stage title and set stage resizable to false.

--- SimpleBankAccount/src/simplebankaccount/SimpleBankAccount.java
+++ SimpleBankAccount/src/simplebankaccount/SimpleBankAccount.java
@@ -14,7 +14,8 @@ public class SimpleBankAccount extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.setResizable(false);
+        stage.setTitle("SimpleBankAccount");
         stage.setScene(scene);
         stage.show();
     }
